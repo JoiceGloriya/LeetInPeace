4ab35085a5120c41111a9e43508961de3dc68104
Joice Gloriya <joicegloriyaa@gmail.com>
2025-01-17 10:15:36 +0530

    Time: 0 ms (100%), Space: 44.5 MB (50.24%) - LeetHub

---
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -0,0 +1,60 @@
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    public Node copyRandomList(Node head) {
+        if(head == null) return null;
+        createCopyNodes(head);
+        connectRandomPointers(head);  
+        return connectNextPointers(head);
+    }
+
+    private void createCopyNodes(Node head) {
+        Node temp = head;
+        while(temp != null) {
+            Node copyNode = new Node(temp.val);
+            copyNode.next = temp.next;
+            temp.next = copyNode;
+            temp = temp.next.next;
+        } 
+    }
+
+    private void connectRandomPointers(Node head) {
+        Node temp = head;
+        while(temp != null) {
+            Node copyNode = temp.next;
+            if(temp.random == null) copyNode.random = null;
+            else copyNode.random = temp.random.next;
+            temp = temp.next.next;
+        }
+    }
+
+    private Node connectNextPointers(Node head) {
+        Node dummyHead = new Node(-1);
+        Node temp = head;
+        Node dTemp = dummyHead;
+        while(temp != null) {
+            Node copyNode = temp.next;
+            dTemp.next = copyNode;
+            temp.next = copyNode.next;
+            if(copyNode.next != null) copyNode.next = copyNode.next.next;
+            temp = temp.next;
+            dTemp = dTemp.next;
+        }
+        return dummyHead.next;
+    }
+
+
+}
