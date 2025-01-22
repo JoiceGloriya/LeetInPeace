f5d696644ddbaa3ed962ec37e7ff0f4ee4de6252
Joice Gloriya <joicegloriyaa@gmail.com>
2025-01-22 18:06:51 +0530

    Time: 0 ms (100%), Space: 40.3 MB (79.34%) - LeetHub

--- 2050-count-good-numbers/2050-count-good-numbers.java
+++ 2050-count-good-numbers/2050-count-good-numbers.java
@@ -1,5 +1,7 @@
 class Solution {
     public int countGoodNumbers(long n) {
+    // evens  = 0, 2, 4, 6, 8  => 5 evens
+    // primes = 2, 3, 5, 7     => 4 primes
     long MOD = 1000000007;
     long odd = n / 2;
     long even = (n / 2) + (n % 2);
@@ -7,12 +9,22 @@ class Solution {
 }
 
 private long modularExponentiation(long base, long exp, long mod) {
-    if (exp == 0) return 1;
-    long half = modularExponentiation(base, exp / 2, mod);
-    long result = (half * half) % mod;
-    if (exp % 2 != 0) {
-        result = (result * base) % mod;
+    // if (exp == 0) return 1;
+    // long half = modularExponentiation(base, exp / 2, mod);
+    // long result = (half * half) % mod;
+    // if (exp % 2 != 0) {
+    //     result = (result * base) % mod;
+    // }
+    long MOD = 1000000007;
+    base = base % MOD;
+    if(base == 0) return 0;
+    long result = 1;
+    while(exp > 0) {
+        if(exp % 2 == 1) result = (result * base) % 1000000007; 
+        base = (base * base) % 1000000007;
+        exp = exp / 2;
     }
+
     return result;
 }
 
