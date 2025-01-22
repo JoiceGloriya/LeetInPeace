5d927ad8917bb3e6fd37366c565bc62e4c932c0f
Joice Gloriya <joicegloriyaa@gmail.com>
2025-01-22 18:28:04 +0530

    Time: 0 ms (100%), Space: 40.4 MB (64.67%) - LeetHub

--- 2050-count-good-numbers/2050-count-good-numbers.java
+++ 2050-count-good-numbers/2050-count-good-numbers.java
@@ -1,30 +1,33 @@
 class Solution {
     public int countGoodNumbers(long n) {
-    // evens  = 0, 2, 4, 6, 8  => 5 evens
-    // primes = 2, 3, 5, 7     => 4 primes
-    long MOD = 1000000007;
-    long odd = n / 2;
-    long even = (n / 2) + (n % 2);
-    return (int) ((modularExponentiation(5, even, MOD) * modularExponentiation(4, odd, MOD)) % MOD);
-}
-
-private long modularExponentiation(long base, long exp, long mod) {
-    // if (exp == 0) return 1;
-    // long half = modularExponentiation(base, exp / 2, mod);
-    // long result = (half * half) % mod;
-    // if (exp % 2 != 0) {
-    //     result = (result * base) % mod;
-    // 
-    // base = base % 1000000007;
-    // if(base == 0) return 0;
-    long result = 1;
-    while(exp > 0) {
-        if(exp % 2 == 1) result = (result * base) % 1000000007; 
-        base = (base * base) % 1000000007;
-        exp = exp / 2;
+        // evens = 0, 2, 4, 6, 8 => 5 evens
+        // primes = 2, 3, 5, 7 => 4 primes
+        long MOD = 1000000007;
+        long odd = n / 2;
+        long even = (n / 2) + (n % 2);
+        return (int) ((modularExponentiation(5, even, MOD) * modularExponentiation(4, odd, MOD)) % MOD);
     }
 
-    return result;
-}
+    private long modularExponentiation(long base, long exp, long mod) {
+        if (exp == 0) return 1;
+        // long result = 1;
+        long half = modularExponentiation(base, exp / 2, mod);
+        half = (half * half) % mod;
+        if (exp % 2 != 0) 
+            half = (half * base) % mod;
+        return half;
+        //
+        // Iterative:
+        // long result = 1;
+        // while (exp > 0) {
+        //     if (exp % 2 == 1)
+        //         result = (result * base) % 1000000007;
+        //     base = (base * base) % 1000000007;
+        //     exp = exp / 2;
+            
+        // }
+
+        // return result;
+    }
 
 }
