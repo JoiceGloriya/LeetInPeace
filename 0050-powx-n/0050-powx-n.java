f3cf306761ae1c1e85e05b9fef24511e79098b2f
Joice Gloriya <joicegloriyaa@gmail.com>
2025-02-06 20:28:54 +0530

    Time: 0 ms (100%), Space: 42.5 MB (36.79%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,9 +1,7 @@
 class Solution {
     public double myPow(double x, int n) {
-        if (x == 1)
-            return 1;
-        if (x == 0)
-            return 0;
+        if (x == 1 || x == 0)
+            return x;
         double ans = 1.0;
         long nn = n;
         if (nn < 0)
