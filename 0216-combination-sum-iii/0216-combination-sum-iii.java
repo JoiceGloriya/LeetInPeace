e7099139deb810168d0d37528480b0ce4ffd86f4
Joice Gloriya <joicegloriyaa@gmail.com>
2025-02-08 18:51:51 +0530

    Time: 0 ms (100%), Space: 41.1 MB (60.31%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.java
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -4,6 +4,9 @@ class Solution {
         backtrack(1, new ArrayList<>(), k, n, list);
         return list;
     }
+    
+// TC -> O(2^9 * k) = O(512 * k) ~= O(k) where k is some constant <= 9 
+// SC -> prd of number of combinations and size of each comb. O(2^9 * k) ~= O(k) k is some constant <= 9 and 'k' is recursive depth
 
     void backtrack(int startIndex, List<Integer> a, int k, int n, List<List<Integer>> list) {
         if (a.size() == k && n == 0) {
@@ -13,11 +16,11 @@ class Solution {
         if (a.size() == k)
             return;
         for (int i = startIndex; i <= 9; i++) {
-            if (n - i >= 0) {
+            // if (n - i >= 0) {
                 a.add(i);
                 backtrack(i + 1, a, k, n - i, list);
                 a.remove(a.size() - 1);
-            }
+            // }
         }
     }
 }
