93d585a1dd088efed893b340d284465b5f19b822
Joice Gloriya <joicegloriyaa@gmail.com>
2025-02-13 21:49:14 +0530

    Time: 6 ms (2.23%), Space: 44.8 MB (6.53%) - LeetHub

--- 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.java
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.java
@@ -2,31 +2,34 @@ class Solution {
     public int[] finalPrices(int[] prices) {
         Stack<Integer> stk = new Stack<>();
         List<Integer> list = new ArrayList<>();
-        for(int i = prices.length - 1; i >= 0 ; i --) {
-            while(!stk.isEmpty() && prices[i] < stk.peek())
+        for (int i = prices.length - 1; i >= 0; i--) {
+            while (!stk.isEmpty() && prices[i] < stk.peek())
                 stk.pop();
-            if(stk.isEmpty())
+            if (stk.isEmpty())
                 list.add(prices[i]);
-            else list.add(prices[i] - stk.peek());
+            else
+                list.add(prices[i] - stk.peek());
             stk.push(prices[i]);
         }
-        reverse(list);
+        // reverse(list);
         int[] ans = new int[prices.length];
-        for(int i = 0; i < prices.length; i ++)
-            ans[i] = list.get(i);
+        for (int i = 0; i < prices.length; i++) {
+            ans[i] = list.get(list.size() - 1);
+            list.remove(list.size() - 1);
+        }
+
         return ans;
     }
 
-    void reverse(List<Integer> list) {
-        int start = 0;
-        int end = list.size() - 1;
-        while(start < end) {
-            int temp = list.get(start);
-            list.set(start, list.get(end));
-            list.set(end, temp);
+    // void reverse(List<Integer> list) {
+    // int start = 0;
+    // int end = list.size() - 1;
+    // while(start < end) {
+    // int temp = list.get(start);
+    // list.set(start, list.get(end));
+    // list.set(end, temp);
 
-            start++;
-            end--;
-        }
-    }
+    // start++;
+    // end--;
+    // }
 }
