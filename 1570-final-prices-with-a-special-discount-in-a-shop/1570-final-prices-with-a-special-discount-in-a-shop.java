fb999b2d3bce7957264ed7c78d0e0080e9ce3e1b
Joice Gloriya <joicegloriyaa@gmail.com>
2025-02-13 21:46:27 +0530

    Time: 6 ms (2.23%), Space: 44.5 MB (6.53%) - LeetHub

---
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        Stack<Integer> stk = new Stack<>();
+        List<Integer> list = new ArrayList<>();
+        for(int i = prices.length - 1; i >= 0 ; i --) {
+            while(!stk.isEmpty() && prices[i] < stk.peek())
+                stk.pop();
+            if(stk.isEmpty())
+                list.add(prices[i]);
+            else list.add(prices[i] - stk.peek());
+            stk.push(prices[i]);
+        }
+        reverse(list);
+        int[] ans = new int[prices.length];
+        for(int i = 0; i < prices.length; i ++)
+            ans[i] = list.get(i);
+        return ans;
+    }
+
+    void reverse(List<Integer> list) {
+        int start = 0;
+        int end = list.size() - 1;
+        while(start < end) {
+            int temp = list.get(start);
+            list.set(start, list.get(end));
+            list.set(end, temp);
+
+            start++;
+            end--;
+        }
+    }
+}
