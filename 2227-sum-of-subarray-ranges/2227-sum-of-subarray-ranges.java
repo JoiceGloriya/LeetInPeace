bf8d00530a9ffecad53a05df77a0416b44c7f95a
Joice Gloriya <joicegloriyaa@gmail.com>
2025-03-06 07:18:48 +0530

    Time: 43 ms (18.75%), Space: 44.3 MB (79.47%) - LeetHub

--- 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.java
+++ 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.java
@@ -59,7 +59,7 @@ class Solution {
         int n = arr.length;
         Stack<Integer> st = new Stack<>();
         for (int i = n - 1; i >= 0; i--) {
-            while (!st.isEmpty() && arr[st.peek()] < arr[i])
+            while (!st.isEmpty() && arr[st.peek()] <= arr[i])
                 st.pop();
             nge[i] = st.empty() ? n : st.peek();
             st.push(i);
@@ -72,7 +72,7 @@ class Solution {
         int n = arr.length;
         Stack<Integer> st = new Stack<>();
         for (int i = 0; i < n; i++) {
-            while (!st.isEmpty() && arr[st.peek()] <= arr[i]) {
+            while (!st.isEmpty() && arr[st.peek()] < arr[i]) {
                 st.pop();
             }
             pgee[i] = st.isEmpty() ? -1 : st.peek();
