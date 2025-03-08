e5f56f5a847e3c699a2753186b9ddeecf63575f9
Joice Gloriya <joicegloriyaa@gmail.com>
2025-03-08 20:08:09 +0530

    Time: 63 ms (73.75%), Space: 57.5 MB (52.24%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,11 +1,11 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
         // Optimal approach without any pre-computation of nse[] and pse[]
-        //while traversing in forward direction itself, i can get hold of pse as well as nse cuz, if i wanna pop an element inorder to make the stk strictly increasing (which is essential in case of pse) it means that the ith element is nse of the element that I wanna pop! 
+        //while traversing in forward direction itself, i can get hold of pse as well as nse cuz, if i wanna pop an element inorder to make the stk strictly increasing (which is essential in case of pse) which means that the ith element is nse of the element that I wanna pop! 
         Stack<Integer> stk = new Stack<>();
         int maxArea = 0;
         for(int i = 0; i < heights.length; i ++) {
-            while(!stk.isEmpty() && heights[stk.peek()] >= heights[i]) {
+            while(!stk.isEmpty() && heights[stk.peek()] > heights[i]) {
                 int index = stk.pop();
                 int nse = i;
                 int pse = stk.isEmpty() ? -1 : stk.peek();
