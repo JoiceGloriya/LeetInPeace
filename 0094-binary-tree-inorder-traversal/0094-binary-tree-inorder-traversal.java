053e6f8c726b817e96adcc949840d197a1d206f2
Joice Gloriya <joicegloriyaa@gmail.com>
2025-05-26 21:29:04 +0530

    Time: 0 ms (100%), Space: 41.7 MB (64.86%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -13,22 +13,51 @@
  *     }
  * }
  */
+ 
+// class Solution {
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//         List<Integer> list = new ArrayList<>();
+//         if (root == null)
+//             return list;
+//         Stack<TreeNode> stk = new Stack<>();
+//         TreeNode curr = root;
+//         while (curr != null || !stk.isEmpty()) {
+//             while (curr != null) {
+//                 stk.push(curr);
+//                 curr = curr.left;
+//             }
+//             curr = stk.pop();
+//             list.add(curr.val);
+//             curr = curr.right;
+//         }
+//         return list;
+//     }
+// }
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> list = new ArrayList<>();
-        if (root == null)
-            return list;
-        Stack<TreeNode> stk = new Stack<>();
         TreeNode curr = root;
-        while (curr != null || !stk.isEmpty()) {
-            while (curr != null) {
-                stk.push(curr);
-                curr = curr.left;
+        while(curr != null) {
+            if(curr.left == null) {
+                list.add(curr.val);
+                curr = curr.right;
             }
-            curr = stk.pop();
-            list.add(curr.val);
-            curr = curr.right;
+            else {
+                TreeNode prev = curr.left;
+                while(prev.right != null && prev.right != curr) {
+                    prev = prev.right;
+                }
+                if(prev.right == null) { //thread it
+                    prev.right = curr;
+                    curr = curr.left;
+                }
+                else {//unthread it, print it and move to RST
+                    prev.right = null;
+                    list.add(curr.val);
+                    curr = curr.right;
+                }
+            }
         }
-        return list;
+    return list;
     }
 }
